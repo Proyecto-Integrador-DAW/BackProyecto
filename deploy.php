70ac263e06ff65a3bf8731c2882757ed88c100b4
ALEXl0CO <alexadrialvaro@gmail.com>
2025-02-21 12:46:50 +0100

    MOD deploy

--- deploy.php
+++ deploy.php
@@ -25,7 +25,7 @@
 
     task('deploy:backend', function () {
         run('cd {{deploy_path}}/current && composer install --optimize-autoloader');
-        run('cd {{deploy_path}}/current && sudo npm install && npm run build');
+        run('cd {{deploy_path}}/current && npm install && npm run build');
         run('cd {{deploy_path}}/current && php artisan cache:clear && php artisan config:cache');
     });
     
@@ -36,7 +36,6 @@
     
     task('deploy:migration', function () {
         run('cd {{deploy_path}}/current && php artisan migrate:refresh --force');
-        run('cd {{deploy_path}}/current && php artisan db:seed');
     });
 
     task('deploy:swagger', function () {
