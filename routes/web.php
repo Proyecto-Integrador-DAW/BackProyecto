330917f1f1fa09c08665a49b3491760343bffe30
ALEXl0CO <alexadrialvaro@gmail.com>
2025-02-21 12:37:12 +0100

    MOD web

--- routes/web.php
+++ routes/web.php
@@ -27,72 +27,27 @@
 
     Route::middleware('auth')->group(function () {
 
-        Route::resource('/patients', PatientController::class)->middleware(RoleMiddleware::class.':administrador,coordinador')
-            ->names([
-                'index' => 'patients.web.index',
-                'create' => 'patients.web.create',
-                'store' => 'patients.web.store',
-                'show' => 'patients.web.show',
-                'edit' => 'patients.web.edit',
-                'update' => 'patients.web.update',
-                'destroy' => 'patients.web.destroy',
-            ]);
+        Route::resource('/patients', PatientController::class)->middleware(RoleMiddleware::class.':administrador,coordinador');
         Route::patch('/patients/{id}/restore', [PatientController::class, 'restore'])->middleware(RoleMiddleware::class.':administrador,coordinador')->name('patients.restore');
         Route::delete('/patients/{id}/force-delete', [PatientController::class, 'forceDelete'])->name('patients.forceDelete');
 
 
-        Route::resource('/zones', ZoneController::class)->middleware(RoleMiddleware::class.':administrador,coordinador')
-            ->names([
-                'index' => 'zones.web.index',
-                'create' => 'zones.web.create',
-                'store' => 'zones.web.store',
-                'show' => 'zones.web.show',
-                'edit' => 'zones.web.edit',
-                'update' => 'zones.web.update',
-                'destroy' => 'zones.web.destroy',
-            ]);
+        Route::resource('/zones', ZoneController::class)->middleware(RoleMiddleware::class.':administrador,coordinador');
         Route::patch('/zones/{id}/restore', [ZoneController::class, 'restore'])->middleware(RoleMiddleware::class.':administrador,coordinador')->name('zones.restore');
         Route::delete('/zones/{id}/force-delete', [ZoneController::class, 'forceDelete'])->name('zones.forceDelete');
 
 
-        Route::resource('/teleoperators', TeleoperatorController::class)->middleware(RoleMiddleware::class.':administrador,coordinador')
-            ->names([
-                'index' => 'teleoperators.web.index',
-                'create' => 'teleoperators.web.create',
-                'store' => 'teleoperators.web.store',
-                'show' => 'teleoperators.web.show',
-                'edit' => 'teleoperators.web.edit',
-                'update' => 'teleoperators.web.update',
-                'destroy' => 'teleoperators.web.destroy',
-            ]);
+        Route::resource('/teleoperators', TeleoperatorController::class)->middleware(RoleMiddleware::class.':administrador,coordinador');
         Route::patch('/teleoperators/{id}/restore', [TeleoperatorController::class, 'restore'])->middleware(RoleMiddleware::class.':administrador,coordinador')->name('teleoperators.restore');
         Route::delete('/teleoperators/{id}/force-delete', [TeleoperatorController::class, 'forceDelete'])->name('teleoperators.forceDelete');
 
 
-        Route::resource('/languages', LanguageController::class)->middleware(RoleMiddleware::class.':administrador,coordinador')
-            ->names([
-                'index' => 'languages.web.index',
-                'create' => 'languages.web.create',
-                'store' => 'languages.web.store',
-                'show' => 'languages.web.show',
-                'edit' => 'languages.web.edit',
-                'update' => 'languages.web.update',
-                'destroy' => 'languages.web.destroy',
-            ]);
+        Route::resource('/languages', LanguageController::class)->middleware(RoleMiddleware::class.':administrador,coordinador');
         Route::patch('/languages/{id}/restore', [LanguageController::class, 'restore'])->middleware(RoleMiddleware::class.':administrador,coordinador')->name('languages.restore');
         Route::delete('/languages/{id}/force-delete', [LanguageController::class, 'forceDelete'])->name('languages.forceDelete');
 
 
-        Route::resource('/contacts', EmergencyContactController::class)->middleware(RoleMiddleware::class.':administrador,coordinador')
-            ->names([
-                'index' => 'contacts.web.index',
-                'create' => 'contacts.web.create',
-                'store' => 'contacts.web.store',
-                'show' => 'contacts.web.show',
-                'edit' => 'contacts.web.edit',
-                'update' => 'contacts.web.update',
-                'destroy' => 'contacts.web.destroy',
-            ]);
+        Route::resource('/contacts', EmergencyContactController::class)->middleware(RoleMiddleware::class.':administrador,coordinador');
         Route::patch('/contacts/{id}/restore', [EmergencyContactController::class, 'restore'])->middleware(RoleMiddleware::class.':administrador,coordinador')->name('contacts.restore');
         Route::delete('/contacts/{id}/force-delete', [EmergencyContactController::class, 'forceDelete'])->name('contacts.forceDelete');
     });
