0768e01b725c6480d87a42cb98bbbf76a0a58472
ALEXl0CO <alexadrialvaro@gmail.com>
2025-02-21 12:55:16 +0100

    MOD names on routes api.php

--- routes/api.php
+++ routes/api.php
@@ -31,14 +31,70 @@
 
     Route::middleware(['auth:sanctum','api'])->group( function () {
 
-        Route::apiResource('/alerts',  AlertController::class);
-        Route::apiResource('/teleoperators',  TeleoperatorController::class);
-        Route::apiResource('/languages',  LanguageController::class);
-        Route::apiResource('/zones',  ZoneController::class);
-        Route::apiResource('/patients', PatientController::class);
+        Route::apiResource('/alerts',  AlertController::class)
+            ->names([
+                'index' => 'api.alerts.index',
+                'store' => 'api.alerts.store',
+                'show' => 'api.alerts.show',
+                'update' => 'api.alerts.update',
+                'destroy' => 'api.alerts.destroy',
+            ]);
+
+        Route::apiResource('/teleoperators',  TeleoperatorController::class)
+            ->names([
+                'index' => 'api.teleoperators.index',
+                'store' => 'api.teleoperators.store',
+                'show' => 'api.teleoperators.show',
+                'update' => 'api.teleoperators.update',
+                'destroy' => 'api.teleoperators.destroy',
+            ]);
+
+        Route::apiResource('/languages',  LanguageController::class)
+            ->names([
+                'index' => 'api.languages.index',
+                'store' => 'api.languages.store',
+                'show' => 'api.languages.show',
+                'update' => 'api.languages.update',
+                'destroy' => 'api.languages.destroy',
+            ]);
+
+        Route::apiResource('/zones',  ZoneController::class)
+            ->names([
+                'index' => 'api.zones.index',
+                'store' => 'api.zones.store',
+                'show' => 'api.zones.show',
+                'update' => 'api.zones.update',
+                'destroy' => 'api.zones.destroy',
+            ]);
+
+        Route::apiResource('/patients', PatientController::class)
+            ->names([
+                'index' => 'api.patients.index',
+                'store' => 'api.patients.store',
+                'show' => 'api.patients.show',
+                'update' => 'api.patients.update',
+                'destroy' => 'api.patients.destroy',
+            ]);
+
         Route::apiResource('/contacts', EmergencyContactController::class)
-            ->parameter('contacts', 'emergencyContact');
-        Route::apiResource('/calls', CallController::class);
+            ->parameter('contacts', 'emergencyContact')
+            ->names([
+                'index' => 'api.contacts.index',
+                'store' => 'api.contacts.store',
+                'show' => 'api.contacts.show',
+                'update' => 'api.contacts.update',
+                'destroy' => 'api.contacts.destroy',
+            ]);
+
+        Route::apiResource('/calls', CallController::class)
+            ->names([
+                'index' => 'api.calls.index',
+                'store' => 'api.calls.store',
+                'show' => 'api.calls.show',
+                'update' => 'api.calls.update',
+                'destroy' => 'api.calls.destroy',
+            ]);
+
 
         Route::get('/subtypesAlerts', [AlertSubtypeController::class, 'index']);
         Route::get('/typesAlerts', [AlertTypeController::class, 'index']);
